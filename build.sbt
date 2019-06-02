2e01504679beffb70f858efbcd96eac76f920d62
Agus Sentosa H <agus.sentosa787@gmail.com>
2019-06-02 20:53:14 +0700

    Add assembly plugin

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "0.1"
 scalaVersion := "2.12.8"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5"
 libraryDependencies += "com.lihaoyi" %% "fastparse" % "2.1.0"
+
+assemblyJarName in assembly := "markx.jar"
--- src/main/scala/tlx/Main.scala
+++ src/main/scala/tlx/Main.scala
@@ -5,13 +5,26 @@ import java.io.{File, PrintWriter}
 import scala.io.Source
 
 object Main extends App {
-  val inputFile = Source.fromFile(args(0))
-  val outputWriter = new PrintWriter(new File(args(1)))
-  val fileContents = inputFile.getLines.mkString("\n")
-  Compiler.compile(fileContents) match {
-    case Fail(label) => printf("Compile Error: %s\n", label)
-    case Ok(result) => outputWriter.write(result)
-  }
-  outputWriter.close()
-  inputFile.close()
+  if (args.size < 2)
+    {
+      println("Usage: java -jar markx.jar [input] [output]")
+    }
+  else
+    {
+      val inputFile = Source.fromFile(args(0))
+      val outputWriter = new PrintWriter(new File(args(1)))
+
+      printf("Source: %s\n", args(0))
+      printf("Target: %s\n", args(1))
+      printf("Compiling...\n")
+
+      val fileContents = inputFile.getLines.mkString("\n")
+      Compiler.compile(fileContents) match {
+        case Fail(label) => printf("Compile Error: %s\n", label)
+        case Ok(result) => outputWriter.write(result)
+      }
+      outputWriter.close()
+      inputFile.close()
+      printf("Done\n")
+    }
 }
